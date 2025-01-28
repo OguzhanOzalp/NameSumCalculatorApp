e0b0c8c57a71e6fdf0dbb2f9f30f79990a1c6544
oguzh <oguzh@MSI>
2025-01-28 13:09:59 +0100

    Add project files.

---
+++ NameSumCalculatorApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using NameSumCalculator_0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Set console input and output encoding to UTF-8
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        //Runs the tests
+        UnitTest.RunTests();
+
+        Console.Write("Enter a name: ");
+        string name = Console.ReadLine();
+
+        int result = NameSumCalculator.CalculateNameSum(name);
+        Console.WriteLine($"The sum of the letters in \"{name}\" is: {result}");
+    }
+
+}
